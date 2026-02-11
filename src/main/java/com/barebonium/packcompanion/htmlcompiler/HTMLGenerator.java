08129a96fe8dc46a65fe6536452db0bbc298d380
Thermonuclear_Minecraft_Engineer <best1chanel@gmail.com>
2026-02-11 13:42:41 +0100

    fix tiny color

--- src/main/java/com/barebonium/packcompanion/htmlcompiler/HTMLGenerator.java
+++ src/main/java/com/barebonium/packcompanion/htmlcompiler/HTMLGenerator.java
@@ -40,7 +40,7 @@ public class HTMLGenerator {
                 "  body.light-mode .problematic { color: #cf222e; }" +
                 "  .deprecated { color: #ffdc72; font-weight: bold; } " +
                 "  body.light-mode .deprecated { color: #ffa700; }" +
-                "  .code-snippet{font-family: 'Courier New', Courier, monospace; font-size: 85%; background-color: rgba(240, 246, 252, 0.15); padding: 0.2em 0.4em; border-radius: 3px; color: #ff7b72;}"+
+                "  .code-snippet{font-family: 'Courier New', Courier, monospace; font-size: 85%; background-color: rgba(240, 246, 252, 0.15); padding: 0.2em 0.4em; border-radius: 3px; color: #64d3ff;}"+
                 "  body.light-mode .code-snippet{ background-color: rgba(11, 36, 62, 0.9); color: #FFFFFF;}"+
                 "  .config-name {color: #72ffeb;}"+
                 "  body.light-mode .config-name{ color: #129d7d;}"+
